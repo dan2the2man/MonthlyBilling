9e4c1e0bc0607b60d7690da02b7365b0afad174d
joekern2 <116761823+joekern2@users.noreply.github.com>
2023-01-09 15:06:51 -0600

    Add files via upload

--- BillingFuns.py
+++ BillingFuns.py
@@ -104,7 +104,8 @@ def writeToCsv(d, lng, i):
 @st.cache
 def convert_df(df, f):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode(f, 'utf-8')
+    
+    return df.to_csv(f, encoding="utf-8")
 
     
 def getYear(y):
