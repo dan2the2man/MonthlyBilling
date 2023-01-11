9782151fc478153bf2dc50c3d28580512f2cd462
joekern2 <116761823+joekern2@users.noreply.github.com>
2023-01-11 11:26:40 -0600

    Add files via upload

--- BillingFuns.py
+++ BillingFuns.py
@@ -97,6 +97,8 @@ def writeToCsv(d, lng, i, sourcewell):
     df = d[idx]
     if sourcewell:
         df = df[['Serial Number', 'Billing Info']]
+        df['Billing Info'] = df['Billing Info'].str.split('[').str[1]
+        df['Billing Info'] = df['Billing Info'][:-2]
     else:
         df = df[['Serial Number', 'VIN', 'Bill Days', 'Quantity', 'Unit Cost', 'Cost']]
     df.index = np.arange(1, len(df) + 1)
